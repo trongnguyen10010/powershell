4a7d61556f462788ea2792ded6f2184a1cd9d6de
trongnguyen10010 <trong.nguyen93@gmail.com>
2016-11-22 20:13:27 -0500

    tn

---
+++ enable-PSRemoting-office.ps1
@@ -0,0 +1,13 @@
+﻿$list = Get-Content "C:\Users\PStools\winrm-error-office-----.txt"
+
+
+$command_prefix = "psexec.exe \\"
+$command_suffix = " -s powershell Enable-PSRemoting -Force"
+foreach ($pc in $list){
+    $command = $command_prefix + $pc + $command_suffix
+    Write-Host "enabling PSRemoting on" $pc -ForegroundColor Green
+    $command
+    Invoke-Expression -Command $command
+    Invoke-Expression return 
+    
+}
