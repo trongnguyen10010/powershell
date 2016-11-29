e966c931cea62662567b39fa347a3a917c87d415
trong <trongnguyen10010@users.noreply.github.com>
2016-11-29 02:11:40 -0500

    tn

--- office365-add-DLs.ps1
+++ office365-add-DLs.ps1
@@ -1,16 +1,14 @@
 $UserCredential = Get-Credential
+$DLs = Get-Content -Path "N:\path\file.txt"
+$username = "ttn9.jobs@gmail.com"
 
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-
+$Session = New-PSSession 
+-ConfigurationName Microsoft.Exchange
+-ConnectionUri https://outlook.office365.com/powershell-liveid/
+-Credential $UserCredential 
+-Authentication Basic 
+-AllowRedirection
 Import-PSSession $Session
-
-
-####################################################################
-
-$DLs = Get-Content -Path "C:\Users\tnguyen\New employee\hstone.txt"
-$username = "hstone@taskstream.com"
-
-####################################################################
 
 foreach ($DL in $DLs) {
     Add-DistributionGroupMember -Member $username -Identity $DL
