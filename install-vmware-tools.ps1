3339f50812cdc24a75b63908db37ed05282bde5f
trong <trongnguyen10010@users.noreply.github.com>
2016-11-29 02:42:16 -0500

    tn

--- install-vmware-tools.ps1
+++ install-vmware-tools.ps1
@@ -1,19 +1,22 @@
-$cmd = '/S /v "/qn REBOOT=R ADDLOCAL=ALL REMOVE=Hgfs"'
-$source_folder = "\\tnguyen\C$\Users\tnguyen\Vmware-Tools"
+$cmd = 'setup.exe /S /v "/qn REBOOT=R ADDLOCAL=ALL REMOVE=Hgfs"'
 
-$list_of_pcs = "C:\Users\tnguyen\Vmware-Tools\dev-servers.txt"
+$source_folder = "\\network\VMware-tools-folder"
+$list_of_vms = "\\path\to\text.txt"
+$when_the_error_code_is_1_logfile = "\\file.log" # mailto:ttn9.jobs@gmail.com
 
-foreach ($computer in (Get-Content -Path $list_of_pcs)){
-    $dest_folder = "\\$computer\c$\Vmware-Tools"
-    Write-Host -ForegroundColor green $computer
+foreach ($computer in (Get-Content -Path $list_of_vms)){
+    $dest_folder = "\\$computer\Vmware-Tools-installation"
+    
     if (!(Test-Path -Path $dest_folder)) {
         Write-Host "Copying files to $dest_folder"
-        Copy-Item -Path $source_folder -Destination $dest_folder -Recurse #-Verbose 
+        Copy-Item -Path $source_folder -Destination $dest_folder -Recurse 
+        Invoke-Command -ComputerName $computer -ScriptBlock {cmd /c 'C:\Vmware-Tools-installation\install.txt.cmd'} 
+        # install.txt.cmd <= setup.exe /S /v "/qn REBOOT=R ADDLOCAL=ALL REMOVE=Hgfs"
+        Write-Host -ForegroundColor green $computer
+        Remove-Item -Path $dest_folder -Recurse
+            }
+    else {
+        Write-Host -ForegroundColor red $computer
+        $computer | Out-File -append -filepath $when_the_error_code_is_1_logfile
     }
-    
-    Write-Host -ForegroundColor Green "Installing vmware tools"
-    Invoke-Command -ComputerName $computer -ScriptBlock {cmd /c 'C:\Vmware-Tools\install-vmtools.txt.cmd'} 
-
-    Write-Host "Cleaning up $dest_folder"
-    Remove-Item -Path $dest_folder -Recurse #-Verbose
 }
